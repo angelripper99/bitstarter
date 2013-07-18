5856c4ff3d7aed4278c9391a657b8654179b7b58
Angelripper99 <daemonx.d@gmail.com>
2013-07-18 20:52:03 +0000

    changes to web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
      var html = fs.readFileSync(htmlfile).toString()  
-     response.sedn(html);
+     response.send(html);
 });
 
 var port = process.env.PORT || 5000;
